1cad2d99f728e2cbdbbb293316fadcbf7f1a7406
horsicq <horsicq@gmail.com>
2023-11-03 18:04:30 +0100

    Update file(s): 2023-11-03

--- examples/C/simple.c
+++ examples/C/simple.c
@@ -6,7 +6,6 @@ int main(int argc, char* argv[])
     char *pszFileName="C:\\Windows\\notepad.exe";
     //char *pszDatabase = "C:\\db";
     char *pszDatabase = "$data/db";
-    int nHandle=0;
     char *pszResult=0;
     
     pszResult=DIE_ScanFileA(pszFileName, DIE_DEEPSCAN | DIE_HEURISTICSCAN | DIE_RECURSIVESCAN, pszDatabase);
