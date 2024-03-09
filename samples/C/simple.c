95fffb311de8d52869d9f2e00bbbd79c414620b9
horsicq <horsicq@gmail.com>
2024-03-09 02:21:39 +0100

    Add new file(s): 2024-03-09

---
+++ samples/C/simple.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "die.h"
+
+int main(int argc, char* argv[])
+{
+    char *pszFileName="C:\\Windows\\notepad.exe";
+    //char *pszDatabase = "C:\\db";
+    char *pszDatabase = "$data/db";
+    char *pszResult=0;
+    
+    pszResult=DIE_ScanFileA(pszFileName, DIE_DEEPSCAN | DIE_HEURISTICSCAN | DIE_RECURSIVESCAN, pszDatabase);
+    printf("%s",pszResult);
+    
+    DIE_FreeMemoryA(pszResult);
+
+    return 0;
+}
+
