a25dcaf8ec61ad19ac2afcea3c294b37d1eda452
horsicq <horsicq@gmail.com>
2024-03-15 00:06:09 +0100

    Update file(s): 2024-03-15

--- build/build_msvc2013_x86.bat
+++ build/build_msvc2013_x86.bat
@@ -1,10 +1,10 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86
 cd ..
 mkdir tmp_build
-cd Release
+cd tmp_build
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH="C:\Qt\Qt5.6.3\5.6.3\msvc2013" -G "NMake Makefiles" ..
 nmake
 cpack -G ZIP
-xcopy /y /E packages/ ../
 
 cd ..
+xcopy /y /E tmp_build\packages\ packages\
--- build/build_msvc2019_x86.bat
+++ build/build_msvc2019_x86.bat
@@ -1,10 +1,10 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\VC\Auxiliary\Build\vcvarsall.bat" x86
 cd ..
 mkdir tmp_build
-cd Release
+cd tmp_build
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH="C:\Qt\5.15.2\msvc2019" -G "NMake Makefiles" ..
 nmake
 cpack -G ZIP
-xcopy /y /E packages/ ../
 
 cd ..
+xcopy /y /E tmp_build\packages\ packages\
