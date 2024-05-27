fec5c073545ef02732d162a53e262b6ebad95807
horsicq <horsicq@gmail.com>
2024-05-28 01:25:15 +0200

    Update file(s): 2024-05-28

--- build/build_dpkg.sh
+++ build/build_dpkg.sh
@@ -1,1 +1,1 @@
-cd ..mkdir tmp_buildcd tmp_buildcmake -DCMAKE_BUILD_TYPE=Release ..makecpack -G DEBcd ..cp -Rf tmp_build/packages .
+cd ..mkdir tmp_buildcd tmp_buildcmake -DCMAKE_BUILD_TYPE=MinSizeRel ..makecpack -G DEBcd ..cp -Rf tmp_build/packages .
--- build/build_macos.sh
+++ build/build_macos.sh
@@ -1,1 +1,7 @@
-export PATH="/Application/CMake.app/Contents/bin:$PATH"cd ..mkdir tmp_buildcd tmp_buildcmake -DCMAKE_BUILD_TYPE=Release ..make
+# sudo ~/Qt/Tools/CMake/CMake.app/Contents/bin/cmake-gui --install
+cd ..
+mkdir tmp_build
+cd tmp_build
+cmake -DCMAKE_BUILD_TYPE=MinSizeRel -DCMAKE_PREFIX_PATH="~/Qt/5.15.2/clang_64" ..
+make
+Pack -G ZIP
