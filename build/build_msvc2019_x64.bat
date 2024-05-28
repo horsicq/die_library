531ccd767fec85d5c9d39b38a98a70ecb1b69f19
horsicq <horsicq@gmail.com>
2024-05-29 00:58:32 +0200

    Update file(s): 2024-05-29

--- build/build_msvc2019_x64.bat
+++ build/build_msvc2019_x64.bat
@@ -6,7 +6,7 @@ IF EXIST "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\VC\Au
 cd ..
 mkdir tmp_build
 cd tmp_build
-cmake -DCMAKE_BUILD_TYPE=MinSizeRel -DCMAKE_PREFIX_PATH="C:\Qt\5.15.2\msvc2019" -G "NMake Makefiles" ..
+cmake -DCMAKE_BUILD_TYPE=MinSizeRel -DCMAKE_PREFIX_PATH="C:\Qt\5.15.2\msvc2019_64" -G "NMake Makefiles" ..
 nmake
 cpack -G ZIP
 
