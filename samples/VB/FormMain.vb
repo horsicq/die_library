3f413fbb5040bc344435cb8b6f06b631a0f935c7
horsicq <horsicq@gmail.com>
2024-11-03 10:50:32 +0100

    Add new file(s): 2024-11-03

---
+++ samples/VB/FormMain.vb
@@ -0,0 +1,25 @@
+﻿Imports System.Runtime.InteropServices 'for DllImport()
+Imports System.Text
+
+Public Class FormMain
+    <DllImport("die.dll", CharSet:=CharSet.Unicode,
+               CallingConvention:=CallingConvention.Cdecl, EntryPoint:="DIE_VB_ScanFile")>
+    Shared Function DIE_VB_ScanFile(lpFileName As String, nFlags As Integer, lpDataBase As String, lpBuffer As String, nBufferSize As Integer) As Integer
+    End Function
+
+    Private Sub FormMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        TextBoxFileName.Text = "C:\Windows\notepad.exe"
+        TextBoxDataBase.Text = "$app/db"
+    End Sub
+    Private Sub ButtonScan_Click(sender As Object, e As EventArgs) Handles ButtonScan.Click
+        Dim sFileName As String = TextBoxFileName.Text
+        Dim sDataBase As String = TextBoxDataBase.Text
+        Dim nBufferSize = 10000
+        Dim sBuffer As String
+        sBuffer = Space$(nBufferSize)
+        Dim nResult As Integer = DIE_VB_ScanFile(sFileName, 0, sDataBase, sBuffer, nBufferSize - 1)
+        sBuffer = sBuffer.Substring(0, nResult)
+        RichTextBoxResult.Text = sBuffer
+
+    End Sub
+End Class
