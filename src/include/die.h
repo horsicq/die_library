96674074e426671aa3ac05d4f4eb0ff8e68ca4e3
horsicq <horsicq@gmail.com>
2024-12-21 01:00:32 +0100

    Update file(s): 2024-12-21

--- src/include/die.h
+++ src/include/die.h
@@ -40,10 +40,16 @@ int DIE_VB_ScanFile(wchar_t *pwszFileName, unsigned int nFlags, wchar_t *pwszDat
 #ifdef UNICODE
 #define DIE_ScanFile DIE_ScanFileW
 #define DIE_ScanMemory DIE_ScanMemoryW
+#define DIE_LoadDatabase DIE_LoadDatabaseW
+#define DIE_ScanFileEx DIE_ScanFileExW
+#define DIE_ScanMemoryEx DIE_ScanMemoryExW
 #define DIE_FreeMemory DIE_FreeMemoryW
 #else
 #define DIE_ScanFile DIE_ScanFileA
 #define DIE_ScanMemory DIE_ScanMemoryA
+#define DIE_LoadDatabase DIE_LoadDatabaseA
+#define DIE_ScanFileEx DIE_ScanFileExA
+#define DIE_ScanMemoryEx DIE_ScanMemoryExA
 #define DIE_FreeMemory DIE_FreeMemoryA
 #endif
 
